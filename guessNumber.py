1ab0cdb9e558ef0e9349404ba6670dd3edf3b85a
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-02-06 00:46:07 +0300

    add number guessing game file

---
+++ guessNumber.py
@@ -0,0 +1,59 @@
+from random import sample, seed, choice
+import string
+
+alphabet_lowercase = string.ascii_lowercase
+alphabet_uppercase = string.ascii_uppercase
+punctuations = string.punctuation
+
+alphabet_list = list(alphabet_lowercase + alphabet_uppercase + punctuations + ' ')
+
+
+def guessNumber(guess_number, n):
+    if len(guess_number) == 4:
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+        seed(n)
+        current_list = sample(numbers, 4)
+        current_string = ''.join(str(s) for s in current_list)
+        # print(current_string)
+
+        result = []
+        for i in range(len(guess_number)):
+            if guess_number[i] == current_string[i]:
+                result.append('+')
+            elif guess_number[i] in current_string:
+                result.append('-')
+
+        result_string = ''.join(str(r) for r in result)
+
+        return result_string, current_string
+
+    else:
+        return 'you should write 4 characters.'
+
+
+guess_list = []
+seed_number = int(choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+size = 5
+for g in range(0, size):
+    guess = input()
+    guess_list.append(str(guess))
+    number = guessNumber(guess, seed_number)
+    if number == 'you should write 4 characters.':
+        result_list = number
+    else:
+        result_number = number[1]
+        result_list = number[0]
+    x = str(guess).isdigit()
+    if x:
+        if str(guess) in guess_list and result_list == '++++':
+            print('YOU GUESSED IT!!')
+            break
+        if str(guess) in guess_list and result_list != '++++':
+            if int(g) == int(4):
+                print('FAILED!!')
+                print('The number was ', result_number)
+            elif int(g) != int(4):
+                print(result_list)
+    else:
+        print('type error. you should write a number.')
