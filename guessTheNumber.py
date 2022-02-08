a8e7467e95c77e7911dc0e3ef7e7af1c5c2dbdbe
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-02-08 17:07:05 +0300

    update guessTheNumber.py

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -29,31 +29,35 @@ def guessNumber(guess_number, n):
         return result_string, current_string
 
     else:
-        return 'you should write 4 characters.'
+        return 'You should write 4 characters.'
 
 
 guess_list = []
 seed_number = int(choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 size = 5
 for g in range(0, size):
-    guess = input()
-    guess_list.append(str(guess))
-    number = guessNumber(guess, seed_number)
-    if number == 'you should write 4 characters.':
-        result_list = number
-    else:
-        result_number = number[1]
-        result_list = number[0]
-    x = str(guess).isdigit()
-    if x:
-        if str(guess) in guess_list and result_list == '++++':
-            print('YOU GUESSED IT!!')
-            break
-        if str(guess) in guess_list and result_list != '++++':
-            if int(g) == int(4):
-                print('FAILED!!')
-                print('The number was ', result_number)
-            elif int(g) != int(4):
-                print(result_list)
-    else:
-        print('type error. you should write a number.')
+    try:
+        guess = input()
+        guess_list.append(str(guess))
+        number = guessNumber(guess, 0)
+        if number == 'You should write 4 characters.':
+            result_list = number
+        else:
+            result_number = number[1]
+            result_list = number[0]
+        x = str(guess).isdigit()
+        if x:
+            if str(guess) in guess_list and result_list == '++++':
+                print('YOU GUESSED IT RIGHT!!')
+                break
+            if str(guess) in guess_list and result_list != '++++':
+                if int(g) == int(4):
+                    print('You failed. The number was', result_number,'.')
+                elif int(g) != int(4):
+                    print(result_list)
+        else:
+            print('Type error. You should write a number.')
+    except NameError:
+        print('FAILED!!! You did not write 4 characters.')
+
+
